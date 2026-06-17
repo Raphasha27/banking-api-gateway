a1f2ef3c6bba62a7a6a790fbf4da13c45ef43a75
Raphasha27 <raphashakoketso69@gmail.com>
2026-06-17 17:12:24 +0200

    feat: add models, repositories, services, controllers, and security config

---
+++ src/test/java/com/banking/bankingapigateway/BankingApiGatewayApplicationTests.java
@@ -0,0 +1,10 @@
+package com.banking.bankingapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest @ActiveProfiles("test")
+class BankingApiGatewayApplicationTests {
+    @Test void contextLoads() {}
+}
